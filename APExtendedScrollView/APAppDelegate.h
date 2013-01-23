34d866e7acf97859ac81e7e66f1c646050ad5b66
Andrzej <andrzej@iMac-Andrzej.local>
2013-01-23 17:31:22 +0100

    Initial Commit

---
+++ APExtendedScrollView/APAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  APAppDelegate.h
+//  APExtendedScrollView
+//
+//  Created by Andrzej on 23.01.2013.
+//  Copyright (c) 2013 apuczyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class APViewController;
+
+@interface APAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) APViewController *viewController;
+
+@end
---
+++ APExtendedScrollView/APViewController.h
@@ -0,0 +1,13 @@
+//
+//  APViewController.h
+//  APExtendedScrollView
+//
+//  Created by Andrzej on 23.01.2013.
+//  Copyright (c) 2013 apuczyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APViewController : UIViewController
+
+@end
